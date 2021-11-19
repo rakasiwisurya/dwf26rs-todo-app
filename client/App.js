baafa62b3c52e8dbdc343c9d3a5d9867bd1b3cb5
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-11-20 03:32:51 +0700

    done client todo app

--- client/App.js
+++ client/App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+
+// Import 'NativeBaseProvider' component
+import { NativeBaseProvider, extendTheme } from "native-base";
+
+// Import font with Expo
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  BalsamiqSans_400Regular,
+  BalsamiqSans_700Bold,
+} from "@expo-google-fonts/balsamiq-sans";
+
+// Import Container
+import Container from "./Container";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  // Load Font with Expo
+  let [fontsLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_700Bold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // Setup Font
+  const fontConfig = {
+    BalsamiqSans: {
+      400: {
+        normal: "BalsamiqSans_400Regular",
+      },
+      700: {
+        normal: "BalsamiqSans_700Bold",
+      },
+    },
+  };
+
+  // Setup Custome Theme
+  const customeColor = {
+    primary: {
+      50: "#E3F2F9",
+      100: "#C5E4F3",
+      200: "#A2D4EC",
+      300: "#7AC1E4",
+      400: "#47A9DA",
+      500: "#0088CC",
+      600: "#007AB8",
+      700: "#006BA1",
+      800: "#005885",
+      900: "#003F5E",
+    },
+    amber: {
+      400: "#d97706",
+    },
+  };
+
+  // Configuration Native Base Custom Theme
+  const theme = extendTheme({
+    colors: customeColor,
+    fontConfig,
+    fonts: {
+      heading: "BalsamiqSans",
+      body: "BalsamiqSans",
+      mono: "BalsamiqSans",
+    },
+    config: { initialColorMode: "dark" },
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NativeBaseProvider theme={theme}>
+        <StatusBar style="light" />
+        <Container />
+      </NativeBaseProvider>
+    );
+  }
+}
---
+++ client/src/config/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+// Create base URL API
+export const API = axios.create({
+  baseURL: "https://6197c01b164fa60017c22de0.mockapi.io/api/v1/",
+});
